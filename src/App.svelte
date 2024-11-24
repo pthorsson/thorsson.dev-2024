45c2f1f812b33ca3972a8a8a6ec3b7d4032e3431
Patrik Thorsson <pthorsson@fastmail.com>
2024-11-24 12:45:14 +0100

    main page layout implementation

--- src/App.svelte
+++ src/App.svelte
@@ -1,49 +1,58 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg';
-  import viteLogo from '/vite.svg';
-  import Counter from './lib/Counter.svelte';
+  import { onMount } from 'svelte';
+  import Link from './lib/Link.svelte';
+  import Logo from './lib/Logo.svelte';
+
+  let showContent = $state(false);
+  let contentHeight = $state(0);
+  let section: HTMLElement;
+
+  const revealAnimationDuration = 1500;
+
+  onMount(() => {
+    contentHeight = section.scrollHeight;
+  });
 </script>
 
-<main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a
-      href="https://github.com/sveltejs/kit#readme"
-      target="_blank"
-      rel="noreferrer">SvelteKit</a
-    >, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
+<main style="--reveal-animation-duration: {revealAnimationDuration}ms">
+  <Logo
+    onAnimationComplete={() => {
+      showContent = true;
+    }}
+  />
+  <section
+    bind:this={section}
+    class:show={showContent}
+    style="--content-height: {contentHeight}px"
+  >
+    <h2>Freelance software developer based in Gothenburg, Sweden.</h2>
+    <nav>
+      <Link to="https://github.com/pthorsson">Github</Link>
+      <Link to="https://linkedin.com/in/pthorsson">LinkedIn</Link>
+    </nav>
+  </section>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  section {
+    display: flex;
+    flex-direction: column;
+    padding-top: 0;
+    gap: 2rem;
+    opacity: 0;
+    transition: all var(--reveal-animation-duration);
+    height: 0px;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  section.show {
+    opacity: 1;
+    padding-top: 1.75rem;
+    height: var(--content-height);
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  nav {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
   }
 </style>
